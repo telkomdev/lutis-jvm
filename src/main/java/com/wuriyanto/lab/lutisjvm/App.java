bb395f1983068cc188449d686e204ea7bbfe221f
Wuriyanto <wuriyanto48@yahoo.co.id>
2021-10-06 00:00:51 +0700

    remove unused comment

--- src/main/java/com/wuriyanto/lab/lutisjvm/App.java
+++ src/main/java/com/wuriyanto/lab/lutisjvm/App.java
@@ -5,10 +5,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
